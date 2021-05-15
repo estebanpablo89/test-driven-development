11379bfafedc494dc88fe6b0e367a6fe9147453a
estebanpablo89 <estebanpablo89@gmail.com>
2021-05-14 22:54:16 -0500

    delete comments

--- src/user/User.js
+++ src/user/User.js
@@ -3,7 +3,6 @@ const mongoose = require('mongoose');
 const UserSchema = new mongoose.Schema({
   username: {
     type: String,
-    //required: [true, 'Please add a name'],
   },
   email: {
     type: String,
@@ -12,14 +11,12 @@ const UserSchema = new mongoose.Schema({
       /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
       'Please add a valid email',
     ],
-    //unique: true,
   },
 
   password: {
     type: String,
     required: [true, 'Please add a password'],
     minlength: 6,
-    //select: false,
   },
 
   inactive: {
