743bb86ec478dc067a6f45319b5ab14496aa2289
estebanpablo89 <estebanpablo89@gmail.com>
2021-06-10 10:09:22 -0500

    authentication

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const connectDB = require('../config/database');
 const UserRouter = require('./user/UserRouter');
+const AuthRouter = require('./auth/AuthenticationRouter');
 const errorHandler = require('./error/ErrorHandler');
 
 // db
@@ -10,6 +11,7 @@ const app = express();
 app.use(express.json());
 
 app.use(UserRouter);
+app.use(AuthRouter);
 app.use(errorHandler);
 
 module.exports = app;
